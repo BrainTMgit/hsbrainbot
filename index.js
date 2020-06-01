21dfd4a1220917d5d041976f1d59373afcf3261d
BrainTMgit <65611626+BrainTMgit@users.noreply.github.com>
2020-06-01 23:30:21 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const config = require("./config.json"); // в конфиге прописана версия бота и префикс
+const Discord = require("discord.js"); //Подключаем discord.js для дальнейшего использования.
+const client = new Discord.Client(); 
+client.login("NzEwMjA3ODQwMDU2NTA4NTM2.XsfWOA.eUl87pg2crGp1VsDQdU3gjyqMW0"); //Где token пишем токен бота.
+client.on("message", message => { //Пришло сообщение.
+
+if(message.content.toLowerCase()==config.prefix + "test") //Если текст сообщения равен префиксу плюс help, то происходит код в {} Часть кода .toLowerCase() превращает текст в строчный. (Делает из заглавных букв обычные.) 
+{
+message.reply();
+}
+
+
+})
