90f090f14521be199ef7bf1d263024943a6ef9dd
BrainTMgit <65611626+BrainTMgit@users.noreply.github.com>
2020-06-11 17:01:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const config = require("./config.json"); // в конфиге прописан токен и префикс
 const Discord = require("discord.js"); //Подключаем discord.js для дальнейшего использования.
 const client = new Discord.Client(); 
-client.login(config.token); //Где token уже был взят из config.json
 
 client.on("message", message => { //Пришло сообщение.
 
@@ -18,5 +17,7 @@ message.reply("кожаный ублюдок");
 
 //Выдача ролей
 
-})
+});
 
+
+client.login(config.token) //Где token уже был взят из config.json
