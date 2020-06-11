01f33415688a7c727b04cf691ad382eba0a91347
BrainTMgit <65611626+BrainTMgit@users.noreply.github.com>
2020-06-11 08:28:49 +0300

    Токен и префикс перенесен в конфиг

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const config = require("./config.json"); // в конфиге прописана версия бота и префикс
+const { prefix, token } = require("config.json"); // в конфиге прописан токен и префикс
 const Discord = require("discord.js"); //Подключаем discord.js для дальнейшего использования.
 const client = new Discord.Client(); 
-client.login("NzEwMjA3ODQwMDU2NTA4NTM2.XsfWOA.eUl87pg2crGp1VsDQdU3gjyqMW0"); //Где token пишем токен бота.
+client.login(token); //Где token уже был взят из config.json
+
 client.on("message", message => { //Пришло сообщение.
 
 // Хелпер
-if(message.content.toLowerCase()==config.prefix + "help") //Если текст сообщения равен префиксу плюс help, то происходит код в {} Часть кода .toLowerCase() превращает текст в строчный. (Делает из заглавных букв обычные.) 
+if(message.content.toLowerCase()==prefix + "help") //Если текст сообщения равен префиксу плюс help, то происходит код в {} Часть кода .toLowerCase() превращает текст в строчный. (Делает из заглавных букв обычные.) 
 {
 message.reply("сам разбирайся!");
 }
