603565b942a5cf9104b4704e3e6dcab7b46c3cb0
BrainTMgit <65611626+BrainTMgit@users.noreply.github.com>
2020-08-02 11:58:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.once('ready',()=>{console.log("Bot started!");});
 // добавим к верхнему коду строчку, чтобы бот при рестарте отправлял сообщение о рестарте в канал #admin
 client.once('ready',()=>{
 	console.log("Bot started!");
-	const channel = guild.channels.cache.find(ch => ch.name === 'admin');
+	const channel = client.guild.channels.cache.find(ch => ch.name === 'admin');
 	if (!channel) return;
 	channel.send(`BrainBot restarted!`);
 });
