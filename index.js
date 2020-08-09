82463a1488e7e8fbb170e7a24f48093c8a0f9f53
Brain™ <65611626+BrainTMgit@users.noreply.github.com>
2020-08-09 11:34:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ if(message.content.toLowerCase()=="rs u 7"){let user = message.author.id;message
 // калькулятор
 if(message.content.toLowerCase()==config.prefix+"test")
 {
-var rs = message.member.roles.find("722351369662627850");
+var rs = message.member.roles.get("722351369662627850");
 console.log("начало");
 
 	console.log(rs/*message.member.roles*/);
