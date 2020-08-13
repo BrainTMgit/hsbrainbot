79f0c57ab14cc0a8c339e6fdeca96c36339db6db
Brain™ <65611626+BrainTMgit@users.noreply.github.com>
2020-08-13 22:40:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -96,11 +96,10 @@ if(message.content.toLowerCase()=="rs u 7"){let user = message.author.id;message
 	if(message.content.toLowerCase()==config.prefix + "ver") {message.channel.send("Release v242");}
 
 // тестируем проверку ролей
-if(message.content.startsWith()==config.prefix+"test")
+if(message.content.toLowerCase()==config.prefix+"test")
 {
 	console.log("начало, начало, начало, начало, начало, начало, начало, начало, начало, начало, ");
-console.log(message);
-message.reply('true');
+channel.send('test');
 
 	console.log("конец, конец, конец, конец, конец, конец, конец, конец, конец, конец, ");
 }
