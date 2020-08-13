6e08575c109e9824a450e631d40c8b522d05cc5f
Brain™ <65611626+BrainTMgit@users.noreply.github.com>
2020-08-13 15:13:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ if(message.content.toLowerCase()=="rs u 7"){let user = message.author.id;message
 if(message.content.toLowerCase()==config.prefix+"test")
 {
 	console.log("начало, начало, начало, начало, начало, начало, начало, начало, начало, начало, ");
-	console.log(client);
+	console.log(client.channels);
 //if(message.content.toLowerCase()=="rs s 10"){let user = message.author.id;message.guild.member(user).roles.add("722351148463292436");}
 
 
