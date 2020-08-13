e96b90eeef981605f4c170f5ce28a656b259ec98
Brain™ <65611626+BrainTMgit@users.noreply.github.com>
2020-08-13 16:14:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ if(message.content.toLowerCase()=="rs u 8"){let user = message.author.id;message
 if(message.content.toLowerCase()=="rs u 7"){let user = message.author.id;message.guild.member(user).roles.remove("722351455666831410");}
 
 //Текущая версия
-	if(message.content.toLowerCase()==config.prefix + "ver") {message.channel.send("Release v235");}
+	if(message.content.toLowerCase()==config.prefix + "ver") {message.channel.send("Release v239");}
 
 // тестируем проверку ролей
 if(message.content.toLowerCase()==config.prefix+"test")
