f36743f09db407f9cccc8c5145a9a53aa56a7c6b
Brain™ <65611626+BrainTMgit@users.noreply.github.com>
2020-08-14 09:43:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ console.log(i,'=',args[i]);
 
 var x = args[0]/2 + args[1]/2.5 + args[2]/3 + args[3]/4.5 + args[4]/5.5;
 console.log(args[0], args[1], args[2], args[3], args[4]);
-x.toFixed(2);
+x = x.toFixed(2);
 console.log(x);
 //message.reply('получается ${x} штук');
 
