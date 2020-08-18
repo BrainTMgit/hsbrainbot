39f74cfeed190ddb5ce7f3a0510461d8d185b44d
Brain™ <65611626+BrainTMgit@users.noreply.github.com>
2020-08-18 10:54:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,18 +26,7 @@ var quequ = [];
 var q = [];
 
 
-// Сюда засунем функцию удаления из очереди кз для таймера
-//channel = client.channels.cache.get("706060221126017054");
 
-function clearq(){
-for (var i=0; i < q.length; i++ ){
-   var time1 = new Date() - q[i].time;
-   var timeleft = q[i].cooldown - time1/60000;
-   /*if(timeleft<0){
-   channel.send('≤@' + q[i].id + '>, время ожидания закончилось, вы удалены из очереди на кз9');
-   q.splice(i, 1);
-   };*/
-};
 
 
 // Пришло сообщение
@@ -184,6 +173,21 @@ q.splice(place, 1);
 
 
 }); // это остатки от client.on("message", message => { 
+
+// Сюда засунем функцию удаления из очереди кз для таймера
+//channel = client.channels.cache.get("706060221126017054");
+
+function clearq(){
+for (var i=0; i < q.length; i++ ){
+   var time1 = new Date() - q[i].time;
+   var timeleft = q[i].cooldown - time1/60000;
+   if(timeleft<0){
+   channel.send('≤@' + q[i].id + '>, время ожидания закончилось, вы удалены из очереди на кз9');
+   q.splice(i, 1);
+   };
+};
+
+
 
 client.login(config.token); //Где token уже был взят из config.json
 
