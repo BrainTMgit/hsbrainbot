ae5a527fe005d3079e6232a88cbb79061d3db99f
Brain™ <65611626+BrainTMgit@users.noreply.github.com>
2020-08-18 10:49:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,13 +30,12 @@ var q = [];
 //channel = client.channels.cache.get("706060221126017054");
 
 function clearq(){
-for (var i=0; i < q.length; i++ ){/*
+for (var i=0; i < q.length; i++ ){
    var time1 = new Date() - q[i].time;
    var timeleft = q[i].cooldown - time1/60000;
-   timeleft = timeleft.toFixed(1);
    if(timeleft<0){
    channel.send('≤@' + q[i] + '>, время ожидания закончилось, вы удалены из очереди на кз9');
-   q.splice(i, 1);*/
+   q.splice(i, 1);
    };
 };
 
